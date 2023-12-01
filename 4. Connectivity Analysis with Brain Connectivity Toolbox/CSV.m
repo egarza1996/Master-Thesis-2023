1c6de9d9abeff9de5f1695f4950158cbc9c9d2ab
egarza1996 <74057309+egarza1996@users.noreply.github.com>
2023-12-01 18:37:48 +0100

    Update CSV.m

--- 4. Connectivity Analysis with Brain Connectivity Toolbox/CSV.m
+++ 4. Connectivity Analysis with Brain Connectivity Toolbox/CSV.m
@@ -13,11 +13,11 @@ close all;
 
 % Define the root path and the organoid folders
 root_path = '/home/silviu/erika-organoid-data/CODE/CURRENT-FOLDER/PreBCT-and-BCT/loop';
-organoid_folders = {'organoid1', 'organoid2', 'organoid3', 'organoid4', 'organoid5', 'organoid6', 'organoid9'};
+organoid_folders = {'organoid1', 'organoid2', 'organoid4', 'organoid5', 'organoid6', 'organoid9'};
 runs = {'run', 'run_1', 'run_1_segmented', 'run_2', 'run_2_segmented', 'run_3', 'run_3_segmented', 'run_4', 'run_5'};
 
 % Parameter to extract from the BCT.mat file
-parameter_name = 'betweenness_centrality';  % Change this to the parameter you want to extract
+parameter_name = 'sigma';  % Change this to the parameter you want to extract
     % "BCT.mat" contains the following parameters:
     % 'F_dff_dec', 'indices_to_remove', 'correlation_matrix', 'thresholded_matrix', ...
     % 'degree_dist', 'clustering_coeff', 'path_length', 'community_structure', 'betweenness_centrality', 'sigma');
@@ -51,6 +51,11 @@ for i = 1:num_organoids
                 if any(strcmp(mean_parameters, parameter_name))
                     % Calculate the mean value of the parameter
                     value = mean(data.(parameter_name)(:));
+                elseif any(strcmp("F_dff_dec", parameter_name))
+                    % Extract the total number of neurons from "F_dff_dec"
+                    value = data.(parameter_name);
+                    value = size(value);
+                    value = value(1,2);
                 else
                     % Extract the single value parameter directly
                     value = data.(parameter_name);
@@ -70,3 +75,32 @@ end
 % Save the table to a CSV file with the specified parameter name
 csv_file_path = fullfile(root_path, [parameter_name '_values.csv']);
 writetable(extract, csv_file_path, 'WriteRowNames', true);
+
+
+%% Optional: calculating the proportional degree distribution
+% Number of connections the neuron has to other neurons
+% divided by the the number of total connections (i.e. neurons^2) 
+% minus the number of repeated neurons.
+% (Like a correlation matrix, where the diagonal values are not considered.)
+
+% % Load data from CSV files
+% degreeDist = readmatrix('degree_dist_values.csv');
+% neurons = readmatrix('F_dff_dec_values.csv'); % contains the total number of neurons
+% 
+% % Check if the dimensions of the two matrices are the same
+% if all(size(degreeDist) == size(neurons))
+%     % Perform element-wise division
+%     result = degreeDist ./ ((neurons).^2 - neurons) ;
+% 
+%     % Handle any division-by-zero by replacing Inf and NaN with a defined value
+%     % Here, we choose to replace them with zero
+%     result(isinf(result) | isnan(result)) = 0;
+% 
+%     % Append the file name to the directory path
+%     outputFilePath = fullfile(root_path, 'proportional_degree_dist_values.csv');
+% 
+%     % Save the result to the new CSV file at the specified path
+%     writematrix(result, outputFilePath);
+% else
+%     error('The dimensions of the two CSV files do not match.');
+% end
